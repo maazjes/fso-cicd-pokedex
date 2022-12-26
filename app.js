e8e1990fc9d6f0479b154d9b0165cd884ab67372
Marius Hasan <marzusness@gmail.com>
2022-12-26 16:03:10 +0200

    new task

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
-require('dotenv').config()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(express.static("dist"));
 
