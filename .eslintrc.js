245fa866ee59067f1fcaaa1dbc22b2bff9142ee5
Marius Hasan <marzusness@gmail.com>
2022-12-26 16:19:19 +0200

    new task

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "PORT": 5000
   },
   "extends": [
     "eslint:recommended",
