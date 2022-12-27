fd2dd1d9fe03aa3b8dd28c310acc192bae09eb86
Marius Hasan <marzusness@gmail.com>
2022-12-27 23:27:53 +0200

    new

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static("dist"));
 
+app.get('/health', (req, res) => {
+  throw 'error'
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('6')
+})
+
 app.listen(PORT, () => {
   console.log("server started on port 8080");
 });
